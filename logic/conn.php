d9dc5f077d66e9024fa56e6cc5921fd8de0d5bb8
gugunrizal <nugrahagugunrizal@gmail.com>
2025-03-01 15:04:25 +0700

    update env config db

--- logic/conn.php
+++ logic/conn.php
@@ -1,3 +1,3 @@
 <?php
-
-$conn = mysqli_connect("localhost", "root", "", "siyota");
+require_once(dirname(__FILE__) . "/constants.php");
+$conn = mysqli_connect($config["db"]["hostname"], $config["db"]["username"], $config["db"]["password"], $config["db"]["database"]);
---
+++ logic/constants.example.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [
+    "db" => [
+        "hostname" => "",
+        "username" => "",
+        "password" => "",
+        "database" => "",
+    ]
+];
